979df0560274a374fab7c5ff073d1e2cf7546d9d
Piotr Jankowiak <piotr.janusz.jankowiak@gmail.com>
2017-01-16 16:19:46 +0100

    started scrapping articles

--- crawler.py
+++ crawler.py
@@ -4,6 +4,15 @@ url = "http://www.naszemiasto.pl/lista_miejscowosci/"
 web_soup = soup(urllib2.urlopen(url))
 sub_sites = dict()
 
+# ze stron danego wojewodztwa pobrac informacje
+# wybrac tylko te informacje ktore w tresci lub tytule maja miasto
+# lematyzowac
+# Wspolrzedne miast - http://astronomia.zagan.pl/art/wspolrzedne.html
+# mapowac miasto do zbioru info jezeli sa relewantne do zapytania
+# rel
+categories = ["/wydarzenia/urzad-miasta/","/wydarzenia/regionalne/", "/wydarzenia/kryminalne/",
+              "/wydarzenia/rozmaitosci/", "/zmieniamy-miasto/", "/moto/wypadki/"]
+
 my_div = web_soup.find_all(name="ul", attrs={'class': "region"})
 for elem in my_div:
 
@@ -22,5 +31,51 @@ for elem in my_div:
                 a_elem = li.find(name="a")
                 sub_sites[wojewodzka_strona].append( a_elem['href'])
 
-for wojewodztwo in sub_sites.iteritems():
-    print wojewodztwo
+
+
+target_wojewodztwo = "wielkopolskie"
+
+# for wojewodztwo in sub_sites.iteritems():
+#     print wojewodztwo
+articles = []
+for i, elem in enumerate(sub_sites[target_wojewodztwo]):
+    if i == 1:
+        break
+    for ii, category in enumerate(categories):
+        if ii == 1:
+            break
+        final_url = elem+category
+        print final_url
+        web_soup = soup(urllib2.urlopen(final_url))
+        # Najwiekszy news na gorze
+        top_section = web_soup.findAll(name="section", id="rotator-sb")
+        for element in top_section:
+            for a in element.findAll(name="a"):
+                try:
+                    if len(a["href"]) >1:
+                        articles.append(elem+a["href"])
+                except:
+                    pass
+        # Glowna lista pionowa
+        srodek = web_soup.findAll(name="div", id="kol-srodek")
+        for kol in srodek:
+            print srodek
+print len(articles)
+
+
+
+
+
+
+
+
+
+
+"""
+    title = models.CharField(max_length=350)
+    city = models.CharField(max_length=100)
+    tags = models.CharField(max_length=350)
+    link = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now)
+    geom = PointField()
+"""
