9d9eb8ff060f85a8c2856479f0799530ae60693f
Piotr <grael222@gmail.com>
2017-01-20 17:12:19 +0100

    added lemmatisation script

---
+++ lemmatization_python.py
@@ -0,0 +1,20 @@
+import csv
+
+def open_file(filename):
+        f = open(filename, 'r', encoding="utf8") # opens the csv file
+        row_list = f.readlines()
+        f.close()      # closing
+        return row_list
+
+lemmat_filename = 'lemmatization-pl.txt'
+basic_form_dict = dict()
+lematted_rows = open_file(lemmat_filename)
+for row in lematted_rows:
+    row = row.lower().split()
+    basic_form = row[0]
+    grammatical_form = row[1]
+    basic_form_dict[grammatical_form] = basic_form
+
+
+#Remember all words are parsed to lower!!!!
+print (basic_form_dict["gniezna"])
