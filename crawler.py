2ea77d696136f7e88aa72ef639e564f11b07f880
Piotr <grael222@gmail.com>
2017-01-22 22:03:38 +0100

    cities coords done, working on lemma

--- crawler.py
+++ crawler.py
@@ -1,9 +1,15 @@
+# -*- coding: utf-8 -*-
+import csv
+import codecs
 import urllib2
 from bs4 import BeautifulSoup as soup
 import lemmatization_python as lemma
 url = "http://www.naszemiasto.pl/lista_miejscowosci/"
 coords_url = "http://astronomia.zagan.pl/art/wspolrzedne.html"
 strip_signs_list = [',', '.', '\r', '\n', '-', '"', "'", ":", "(", ")", "#", "^", "&", "!", "?", "[", "]"]
+categories = ["/wydarzenia/urzad-miasta/","/wydarzenia/regionalne/", "/wydarzenia/kryminalne/",
+                  "/wydarzenia/rozmaitosci/", "/zmieniamy-miasto/", "/moto/wypadki/"]
+target_wojewodztwo = "wielkopolskie"
 # ze stron danego wojewodztwa pobrac informacje
 # lematyzowac informacje
 #     osobno zapisac wyrazy z duzymy literami,
@@ -16,16 +22,32 @@ strip_signs_list = [',', '.', '\r', '\n', '-', '"', "'", ":", "(", ")", "#", "^"
 # mapowac miasto do zbioru info jezeli sa relewantne do zapytania
 # rel
 
+def get_cities_names_and_coords():
+    cities_coords = dict()
+    # f = codecs.open("cities_coords.csv", encoding='utf-8')
+    # for line in f:
+    #     print unicode(line).encode('utf8'), type(unicode(line).encode('utf8'))
+
+    # with open("cities_coords.csv", 'r') as in_file:
+    #     reader = csv.reader(in_file, delimiter=",", encoding='utf-8')
+    #     for row in reader:
+    #         print [ unicode(x).encode('utf8') for x in row]
+    with open("cities_coords.csv", 'r') as in_file:
+        reader = csv.reader(in_file, delimiter=",")
+        for row in reader:
+            row_as_list = list(row)
+            # Mapping city name to coords in format like this: ((19,42), (49,42))
+            cities_coords[row_as_list[0]] = ((row_as_list[1][0:2], row_as_list[1][4:6]), (row_as_list[2][0:2],row_as_list[2][4:6]))
+            # print row_as_list[0],  cities_coords[row_as_list[0]]
+
+            # fmt = u'{:<15}'*len(row_as_list)
+            # print fmt.format(*[s.decode('utf-8') for s in row_as_list])
+    return cities_coords
 def get_articles():
     web_soup = soup(urllib2.urlopen(url), "html.parser")
     sub_sites = dict()
-
-    categories = ["/wydarzenia/urzad-miasta/","/wydarzenia/regionalne/", "/wydarzenia/kryminalne/",
-                  "/wydarzenia/rozmaitosci/", "/zmieniamy-miasto/", "/moto/wypadki/"]
-
     my_div = web_soup.find_all(name="ul", attrs={'class': "region"})
     for elem in my_div:
-
         for li_region in elem.findChildren():
             wojewodzka_strona = ""
             h3_elem = li_region.find_all(name="h3")
@@ -41,10 +63,6 @@ def get_articles():
                     a_elem = li.find(name="a")
                     sub_sites[wojewodzka_strona].append( a_elem['href'])
 
-    target_wojewodztwo = "wielkopolskie"
-
-    # for wojewodztwo in sub_sites.iteritems():
-    #     print wojewodztwo
     articles = []
     for i, elem in enumerate(sub_sites[target_wojewodztwo]):
         if i == 1:
@@ -53,13 +71,8 @@ def get_articles():
             if ii == 1:
                 break
             final_url = elem+category
-            print final_url
-
 
             web_soup = soup(urllib2.urlopen(final_url), "html.parser")
-            # web_soup = soup(open("view-source_chodziez.naszemiasto.pl_wydarzenia_urzad-miasta_.html"), "html.parser")
-            # print web_soup
-
             # Najwiekszy news na gorze
             top_section = web_soup.findAll(name="section", id="rotator-sb")
             for element in top_section:
@@ -101,19 +114,32 @@ def delete_unallowed_signs(text):
         if ch in text:
             text = text.replace(ch, "")
     return text
+
 news_list = get_articles()
 
 news_lemma_dict = dict()  # Mapping news title to {big_letter_words:[], small_letter_words:[]}
-for article in news_list:
-    print article
-    news_lemma_dict[article] = {"upper_case": [], "lower_case": []}
-    article_parser = soup(urllib2.urlopen(article), "html.parser")
-    title = article_parser.findAll(name="h1", class_="matTytul")[0].string
-    print title
-    content = article_parser.findAll(name="div", id="tresc")
-    content = delete_unallowed_signs(replace_br_with_spaces(content[0]))
-    print content
-    break
+# LEMMATIZATION
+# lemma_dict = lemma.get_lemma_dict()
+
+# City names and coordinates mapping.
+city_to_coord_mapping = get_cities_names_and_coords()
+
+
+# city_to_news_mapping = dict()
+# cities_list = []
+# # print lemma_dict["poznania"]
+#
+# for article in news_list:
+#     print article
+#     news_lemma_dict[article] = {"upper_case": [], "lower_case": []}
+#     article_parser = soup(urllib2.urlopen(article), "html.parser")
+#     title = article_parser.findAll(name="h1", class_="matTytul")[0].string
+#     print title
+#     content = article_parser.findAll(name="div", id="tresc")
+#     content = delete_unallowed_signs(replace_br_with_spaces(content[0]))
+#     for word in content.split():
+#         print word
+#     break
 
 
 
--- lemmatization_python.py
+++ lemmatization_python.py
@@ -1,21 +1,22 @@
 import csv
 
-
 def open_file(filename):
         f = open(filename, 'r')  # opens the csv file
         row_list = f.readlines()
         f.close()      # closing
         return row_list
 
-lemma_filename = 'lemmatization-pl.txt'
-basic_form_dict = dict()
-lematted_rows = open_file(lemma_filename)
-for row in lematted_rows:
-    row = row.lower().split()
-    basic_form = row[0]
-    grammatical_form = row[1]
-    basic_form_dict[grammatical_form] = basic_form
+def get_lemma_dict():
+    lemma_filename = 'full_mapping.csv'
+    basic_form_dict = dict()
+    lematted_rows = open_file(lemma_filename)
+    for row in lematted_rows:
+        row = row.lower().split()
+        basic_form = row[1]
+        grammatical_form = row[0]
+        basic_form_dict[grammatical_form] = basic_form
+    return basic_form_dict
 
 
 # Remember all words are parsed to lower case!!!
-print (basic_form_dict["gniezna"])
+# print (basic_form_dict["gniezna"])
